4ef78074a74026d33d6d7587095467fcfa46d4ca
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-02-25 10:33:34 +0100

    added VerticalViewPager and Adapter properties + initialized it in OnCreate method

--- app/src/main/java/com/matt/android/moodtracker_v2/MainActivity.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/MainActivity.java
@@ -3,11 +3,18 @@ package com.matt.android.moodtracker_v2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import fr.castorflex.android.verticalviewpager.VerticalViewPager;
+
 public class MainActivity extends AppCompatActivity {
+    VerticalViewPager verticalViewPager;
+    CustomSwipeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verticalViewPager = (VerticalViewPager) findViewById(R.id.verticalviewpager);
+        adapter = new CustomSwipeAdapter(this);
+        verticalViewPager.setAdapter(adapter);
     }
 }
