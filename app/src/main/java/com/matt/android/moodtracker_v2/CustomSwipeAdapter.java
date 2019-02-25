84e18244894b28ccc650142f1f898b089dc923e0
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-02-25 10:13:20 +0100

    created imageView and added item_view to container

--- app/src/main/java/com/matt/android/moodtracker_v2/CustomSwipeAdapter.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/CustomSwipeAdapter.java
@@ -5,6 +5,7 @@ import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class CustomSwipeAdapter extends PagerAdapter {
     private int [] images_ressources = {R.drawable.smiley_sad,R.drawable.smiley_disappointed,
@@ -29,8 +30,11 @@ public class CustomSwipeAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View item_view = layoutInflater.inflate(R.layout.swipe_layout,container);
-        return super.instantiateItem(container, position);
+        View item_view = layoutInflater.inflate(R.layout.swipe_layout,container, false);
+        ImageView imageView = (ImageView) item_view.findViewById(R.id.swipe_layout_imageView_ID);
+        imageView.setImageResource(images_ressources[position]);
+        container.addView(item_view);
+        return item_view;
     }
 
     @Override
