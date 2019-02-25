6cfad724d2aa163c5220feea6db2c450a4835597
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-02-25 09:50:37 +0100

    added instanciateItem and destroyItem methods

--- app/src/main/java/com/matt/android/moodtracker_v2/CustomSwipeAdapter.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/CustomSwipeAdapter.java
@@ -2,6 +2,7 @@ package com.matt.android.moodtracker_v2;
 
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class CustomSwipeAdapter extends PagerAdapter {
     @Override
@@ -12,5 +13,15 @@ public class CustomSwipeAdapter extends PagerAdapter {
     @Override
     public boolean isViewFromObject(View view, Object object) {
         return false;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        return super.instantiateItem(container, position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
     }
 }
