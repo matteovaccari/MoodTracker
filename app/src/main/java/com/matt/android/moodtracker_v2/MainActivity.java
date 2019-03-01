74e1c87b2859c2d9804a8c51123ca43b88829a48
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-01 11:07:45 +0100

    added OnPageChangeListener to verticalViewPager to detect which smiley is currently playing

--- app/src/main/java/com/matt/android/moodtracker_v2/MainActivity.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/MainActivity.java
@@ -1,24 +1,30 @@
 package com.matt.android.moodtracker_v2;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import fr.castorflex.android.verticalviewpager.VerticalViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SharedPreferences mPreferences;
-    public static final String PREF_KEY_CURRENT_SMILEY ="PREF_KEY_CURRENT_SMILEY";
     private int currentSmiley;
     VerticalViewPager verticalViewPager;
     private Button historyButton;
     CustomSwipeAdapter adapter;
+    private SharedPreferences mPreferences;
+    public static final String PREF_KEY_CURRENT_SMILEY = "PREF_KEY_CURRENT_SMILEY";
+    public static final String PREF_KEY = "PREF_KEY";
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,23 +40,38 @@ public class MainActivity extends AppCompatActivity {
             //Set default position when launching app to Happy Smiley (3)
         verticalViewPager.setCurrentItem(3);
 
-        mPreferences = getPreferences(MODE_PRIVATE);
+        mPreferences = getApplicationContext().getSharedPreferences(PREF_KEY,0); //0 for private mode
+        final SharedPreferences.Editor editor = mPreferences.edit();
 
-        verticalViewPager.setOnClickListener(new View.OnClickListener() {
+       verticalViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener(){
             @Override
-            public void onClick(View v) {
-               currentSmiley = verticalViewPager.getCurrentItem();
-               mPreferences.edit().putInt(PREF_KEY_CURRENT_SMILEY,currentSmiley);
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+            @Override
+            public void onPageSelected(int position) {
+                currentSmiley=position;
+                Toast.makeText(MainActivity.this, "position: "+position, Toast.LENGTH_SHORT).show();
+                editor.putInt(PREF_KEY_CURRENT_SMILEY,currentSmiley); // Storing integer
+                editor.apply(); // commit changes
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
             }
         });
-
-        historyButton.setOnClickListener(new View.OnClickListener() {
+       
+        //Launch MoodHistoryActivity when history button is clicked
+            historyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent historyActivityIntent = new Intent(MainActivity.this,MoodHistoryActivity.class);
                 startActivity(historyActivityIntent);
             }
         });
+
+
+
     }
 
 
