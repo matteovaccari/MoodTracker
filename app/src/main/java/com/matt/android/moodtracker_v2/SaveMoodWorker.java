8634fa1e617c6be6e85d12cfd44e60b335fc037b
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-06 14:34:56 +0100

    added SharedPrefs and mehod saveMoodAtMidnight (in doWork)

--- app/src/main/java/com/matt/android/moodtracker_v2/SaveMoodWorker.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/SaveMoodWorker.java
@@ -1,19 +1,34 @@
 package com.matt.android.moodtracker_v2;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
+import androidx.work.WorkerParameters;
 
 public class SaveMoodWorker extends Worker {
-    @NonNull
 
-        //Worker class with doWork overrided method
+    int lastDayMood;
+    private SharedPreferences mPreferences;
+    public static final String PREF_KEY_CURRENT_SMILEY = "PREF_KEY_CURRENT_SMILEY";
+    public static final String PREF_KEY = "PREF_KEY";
+
+    public SaveMoodWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
     @Override
     public Worker.Result doWork() {
-        Context context = getApplicationContext();
-                //Do Work
+        saveMoodAtMidnight();
         return Result.SUCCESS;
 
+    }
+
+    public void saveMoodAtMidnight() {
+        Context context = getApplicationContext();
+        mPreferences = getApplicationContext().getSharedPreferences(PREF_KEY,0);
+        lastDayMood = mPreferences.getInt(PREF_KEY_CURRENT_SMILEY,15);
     }
 }
