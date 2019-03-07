5216fbb217c0adaa77f89df1042755bad17f056a
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-07 12:07:15 +0100

    relied each layout + each button + each textview to his ID

--- app/src/main/java/com/matt/android/moodtracker_v2/MoodHistoryActivity.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/MoodHistoryActivity.java
@@ -5,13 +5,14 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MoodHistoryActivity extends AppCompatActivity {
 
-    private TextView currentSmileyTextView;
-    private int currentSmileyInt;
     private String currentSmileyString;
+    private int currentSmileyInt;
     private SharedPreferences mPreferences;
     public static final String PREF_KEY_CURRENT_SMILEY = "PREF_KEY_CURRENT_SMILEY";
     public static final String PREF_KEY = "PREF_KEY";
@@ -21,16 +22,42 @@ public class MoodHistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_history);
 
-        currentSmileyTextView = (TextView) findViewById(R.id.currentsmiley_ID);
-
         mPreferences = getApplicationContext().getSharedPreferences(PREF_KEY,0); //0 for private mode
 
-            //Get position from prefs
-       currentSmileyInt = mPreferences.getInt(PREF_KEY_CURRENT_SMILEY,-50);
-        Log.d("Keyname: ", String.valueOf(currentSmileyInt));
-            //Convert it from int to String
-        currentSmileyString = Integer.toString(currentSmileyInt);
-            //Display it
-        currentSmileyTextView.setText(currentSmileyString);
+        RelativeLayout mDayOne = findViewById(R.id.activity_historic_day_one);
+        RelativeLayout mDayTwo = findViewById(R.id.activity_historic_day_two);
+        RelativeLayout mDayThree = findViewById(R.id.activity_historic_day_three);
+        RelativeLayout mDayFour = findViewById(R.id.activity_historic_day_four);
+        RelativeLayout mDayFive = findViewById(R.id.activity_historic_day_five);
+        RelativeLayout mDaySix = findViewById(R.id.activity_historic_day_six);
+        RelativeLayout mDaySeven = findViewById(R.id.activity_historic_day_seven);
+
+        Button mButtonOne = findViewById(R.id.activity_historic_btn_one);
+        Button mButtonTwo = findViewById(R.id.activity_historic_btn_two);
+        Button mButtonThree = findViewById(R.id.activity_historic_btn_three);
+        Button mButtonFour = findViewById(R.id.activity_historic_btn_four);
+        Button mButtonFive = findViewById(R.id.activity_historic_btn_five);
+        Button mButtonSix = findViewById(R.id.activity_historic_btn_six);
+        Button mButtonSeven = findViewById(R.id.activity_historic_btn_seven);
+
+        TextView mTextViewOne = findViewById(R.id.activity_historic_text_one);
+        TextView mTextViewTwo = findViewById(R.id.activity_historic_text_two);
+        TextView mTextViewThree = findViewById(R.id.activity_historic_text_three);
+        TextView mTextViewFour = findViewById(R.id.activity_historic_text_four);
+        TextView mTextViewFive = findViewById(R.id.activity_historic_text_five);
+        TextView mTextViewSix = findViewById(R.id.activity_historic_text_six);
+        TextView mTextViewSeven = findViewById(R.id.activity_historic_text_seven);
+
+        mTextViewOne.setText(getString(R.string.day_1));
+        mTextViewTwo.setText(getString(R.string.day_2));
+        mTextViewThree.setText(getString(R.string.day_3));
+        mTextViewFour.setText(getString(R.string.day_4));
+        mTextViewFive.setText(getString(R.string.day_5));
+        mTextViewSix.setText(getString(R.string.day_6));
+        mTextViewSeven.setText(getString(R.string.day_7));
+
+        RelativeLayout[] layouts = {mDayOne, mDayTwo, mDayThree, mDayFour, mDayFive, mDaySix, mDaySeven};
+        Button[] buttons = {mButtonOne, mButtonTwo, mButtonThree, mButtonFour, mButtonFive, mButtonSix, mButtonSeven};
+
     }
 }
