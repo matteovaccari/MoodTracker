2bc1f2771b85e357f4bdfa22109d71acc1db9823
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-11 12:32:58 +0100

    added saveMood method

--- app/src/main/java/com/matt/android/moodtracker_v2/models/MySharedPreferences.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/models/MySharedPreferences.java
@@ -3,6 +3,8 @@ package com.matt.android.moodtracker_v2.models;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.matt.android.moodtracker_v2.controllers.MainActivity;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -11,6 +13,7 @@ public class MySharedPreferences {
 
     private SharedPreferences mPreferences;
     public static final String PREF_KEY_NAME = "PREF_KEY_NAME";
+    public static final String PREF_KEY_CURRENT_SMILEY = "PREF_KEY_CURRENT_SMILEY";
 
         //Constructor
     public MySharedPreferences(Context context) {
@@ -20,5 +23,9 @@ public class MySharedPreferences {
     private String getMood(Date date) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy",Locale.FRANCE);
         return "Mood at "+ simpleDateFormat.format(date);
+    }
+
+    public void saveMood(Date date, Integer currentSmileyPos ) {
+        mPreferences.edit().putInt(PREF_KEY_CURRENT_SMILEY,currentSmileyPos);
     }
 }
