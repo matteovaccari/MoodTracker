e0863d9dd7f95a0c575f88982c636909ad8c3052
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-11 12:15:12 +0100

    removed Toast message in DOWork method that caused crash

--- app/src/main/java/com/matt/android/moodtracker_v2/workers/SaveMoodWorker.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/workers/SaveMoodWorker.java
@@ -11,34 +11,37 @@ import com.matt.android.moodtracker_v2.controllers.MoodHistoryActivity;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
-public class SaveMoodWorker extends Worker {
+    public class SaveMoodWorker extends Worker {
 
-    private SharedPreferences mPreferences;
-    public static final String PREF_KEY_CURRENT_SMILEY = "PREF_KEY_CURRENT_SMILEY";
-    public static final String PREF_KEY_CURRENT_COMMENT = "PREF_KEY_CURRENT_COMMENT";
-    public static final String PREF_KEY = "PREF_KEY";
+        private SharedPreferences mPreferences;
+        private int temporaryPos;
+        public static final String PREF_KEY_CURRENT_SMILEY = "PREF_KEY_CURRENT_SMILEY";
+        public static final String PREF_KEY_CURRENT_SMILEY_STATIC = "PREF_KEY_CURRENT_SMILEY_STATIC";
+        public static final String PREF_KEY_CURRENT_COMMENT = "PREF_KEY_CURRENT_COMMENT";
+        public static final String PREF_KEY = "PREF_KEY";
 
 
-    public SaveMoodWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
-        super(context, workerParams);
+        public SaveMoodWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+            super(context, workerParams);
+        }
+
+        @NonNull
+        @Override
+        public Worker.Result doWork() {
+            saveMoodInPrefs();
+            return Result.SUCCESS;
+        }
+
+        public void saveMoodInPrefs() {
+            Context context = getApplicationContext();
+            mPreferences = getApplicationContext().getSharedPreferences(PREF_KEY,0);
+                //Take currentSmileyPos and comment (both static) and put them in SharedPrefs
+            temporaryPos = mPreferences.getInt(PREF_KEY_CURRENT_SMILEY_STATIC,-50);
+            mPreferences.edit().putInt(PREF_KEY_CURRENT_SMILEY,temporaryPos).apply();
+            //Comment have to be in prefs to not be deleted by dead of act
+            mPreferences.edit().putString(PREF_KEY_CURRENT_COMMENT,MainActivity.comment).apply();
+        }
     }
-
-    @NonNull
-    @Override
-    public Worker.Result doWork() {
-        saveMoodInPrefs();
-        Toast.makeText(getApplicationContext(),"Current Mood Saved",Toast.LENGTH_SHORT).show();
-        return Result.SUCCESS;
-    }
-
-    public void saveMoodInPrefs() {
-        Context context = getApplicationContext();
-        mPreferences = getApplicationContext().getSharedPreferences(PREF_KEY,0);
-            //Take currentSmileyPos and comment (both static) and put them in SharedPrefs
-        mPreferences.edit().putInt(PREF_KEY_CURRENT_SMILEY,MainActivity.currentSmileyPosition).apply();
-        mPreferences.edit().putString(PREF_KEY_CURRENT_COMMENT,MainActivity.comment).apply();
-    }
-}
 
 
 
