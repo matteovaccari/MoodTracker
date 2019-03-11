01a2796bbe361ade6c742f3e3ff68a08ccad5014
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-11 12:46:10 +0100

    added real content to saveMood method --> take temporaryPos (the one who can be changed at anytime during the day), and put in in the prefs as FINAL pos when the work will be called at 23h59, then put a String in PREFS who contains date+ smileyPos

--- app/src/main/java/com/matt/android/moodtracker_v2/models/MySharedPreferences.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/models/MySharedPreferences.java
@@ -12,8 +12,11 @@ import java.util.Locale;
 public class MySharedPreferences {
 
     private SharedPreferences mPreferences;
+    private int temporaryPos;
+    private String moodPositionInString;
     public static final String PREF_KEY_NAME = "PREF_KEY_NAME";
     public static final String PREF_KEY_CURRENT_SMILEY = "PREF_KEY_CURRENT_SMILEY";
+    public static final String PREF_KEY_CURRENT_SMILEY_STATIC = "PREF_KEY_CURRENT_SMILEY_STATIC";
 
         //Constructor
     public MySharedPreferences(Context context) {
@@ -25,7 +28,14 @@ public class MySharedPreferences {
         return "Mood at "+ simpleDateFormat.format(date);
     }
 
-    public void saveMood(Date date, Integer currentSmileyPos ) {
-        mPreferences.edit().putInt(PREF_KEY_CURRENT_SMILEY,currentSmileyPos);
+    public void saveMood(Date date) {
+            // Get actual smiley position
+        temporaryPos = mPreferences.getInt(PREF_KEY_CURRENT_SMILEY_STATIC,-50);
+            // Put in prefs actual smiley position as Today final position (CURRENT_SMILEY)
+        mPreferences.edit().putInt(PREF_KEY_CURRENT_SMILEY,temporaryPos).apply();
+            //Put current Final Pos in String
+        moodPositionInString = String.valueOf(mPreferences.getInt(PREF_KEY_CURRENT_SMILEY,-50));
+            //Put String in prefs with DATE + mood in String (position from 0 to 4)
+        mPreferences.edit().putString(getMood(date),moodPositionInString);
     }
 }
