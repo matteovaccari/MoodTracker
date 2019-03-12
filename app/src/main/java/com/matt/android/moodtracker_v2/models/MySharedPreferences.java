283aa0564dc09b76346ad0998547c494b1872beb
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-12 16:01:44 +0100

    added getMoodNameForSharing method in Prefs who take currentSmileyPos in MainAct and translate it to corresponding mood name (for sharing message)

--- app/src/main/java/com/matt/android/moodtracker_v2/models/MySharedPreferences.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/models/MySharedPreferences.java
@@ -11,6 +11,7 @@ public class MySharedPreferences {
 
     private SharedPreferences mPreferences;
     public static final String PREF_KEY_NAME = "PREF_KEY_NAME";
+    private String moodInStringForShare;
 
     public MySharedPreferences(Context context) {
         mPreferences = context.getSharedPreferences(PREF_KEY_NAME,Context.MODE_PRIVATE);
@@ -47,5 +48,27 @@ public class MySharedPreferences {
 
     public String getComment (Date date) {
         return mPreferences.getString(getCommentDate(date),null);
+    }
+
+    public String getMoodNameForSharing(int currentPos) {
+
+        switch (currentPos) {
+            case 0:
+                moodInStringForShare = "sad";
+                break;
+            case 1:
+                moodInStringForShare = "dissapointed";
+                break;
+            case 2:
+                moodInStringForShare = "normal";
+                break;
+            case 3:
+                moodInStringForShare = "happy";
+                break;
+            case 4:
+                moodInStringForShare = "supper happy";
+                break;
+        }
+        return moodInStringForShare;
     }
 }
