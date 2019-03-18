aa2a9cf24e029fe75bfadafae73ec39502446f03
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-18 12:05:06 +0100

    added content for History item class (smiley drawable attribut + background color)

--- app/src/main/java/com/matt/android/moodtracker_v2/models/HistoryItem.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/models/HistoryItem.java
@@ -1,4 +1,27 @@
 package com.matt.android.moodtracker_v2.models;
 
 public class HistoryItem {
+    private int smiley;
+    private int backgroundColor;
+
+    public HistoryItem(int smiley, int backgroundColor) {
+        setBackgroundColor(backgroundColor);
+        setSmiley(smiley);
+    }
+
+    public int getSmiley() {
+        return smiley;
+    }
+
+    public void setSmiley(int smiley) {
+        this.smiley = smiley;
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
 }
