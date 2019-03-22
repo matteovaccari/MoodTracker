0b67146334ed0390ddb5078bd8906eea99a51008
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-22 12:30:44 +0100

    reformated HistoryItem --> removed all the statics attribut and put 1 Mood and 1 commetn attribut

--- app/src/main/java/com/matt/android/moodtracker_v2/models/HistoryItem.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/models/HistoryItem.java
@@ -8,19 +8,9 @@
 
 package com.matt.android.moodtracker_v2.models;
 
-import com.matt.android.moodtracker_v2.R;
-
 public abstract class HistoryItem {
 
-    public static int sadSmiley = R.drawable.smiley_sad;
-    public static int disappointedSmiley = R.drawable.smiley_disappointed;
-    public static int normalSmiley = R.drawable.smiley_normal;
-    public static int happySmiley = R.drawable.smiley_happy;
-    public static int superHappySmiley = R.drawable.smiley_super_happy;
-    public static int sadSmileyBackground = R.color.faded_red;
-    public static int disappointedSmileyBackground = R.color.warm_grey;
-    public static int normalSmileyBackground = R.color.cornflower_blue_65;
-    public static int happySmileyBackground = R.color.light_sage;
-    public static int superHappySmileyBackground = R.color.banana_yellow;
+    private Mood mood;
+    private String comment;
 
 }
