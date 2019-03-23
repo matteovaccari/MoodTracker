52127876048383c702451fdcffe47aaeb8ce01fc
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-23 17:32:41 +0100

    added constructors, getters and setters for HistoryItem

--- app/src/main/java/com/matt/android/moodtracker_v2/models/HistoryItem.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/models/HistoryItem.java
@@ -8,9 +8,29 @@
 
 package com.matt.android.moodtracker_v2.models;
 
-public abstract class HistoryItem {
+public class HistoryItem {
 
     private Mood mood;
     private String comment;
 
+    public HistoryItem(Mood mood, String comment) {
+        setComment(comment);
+        setMood(mood);
+    }
+
+    public Mood getMood() {
+        return mood;
+    }
+
+    public void setMood(Mood mood) {
+        this.mood = mood;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
