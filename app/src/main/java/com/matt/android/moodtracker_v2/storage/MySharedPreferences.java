06f5ea2e52f9c799e78f6de84403a10d68b1a8db
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-25 15:04:57 +0100

    added saveHistoryItem and getHistoryItem methods with Json within

--- app/src/main/java/com/matt/android/moodtracker_v2/storage/MySharedPreferences.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/storage/MySharedPreferences.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import com.matt.android.moodtracker_v2.R;
 import com.matt.android.moodtracker_v2.models.HistoryItem;
@@ -19,12 +20,18 @@ import com.google.gson.Gson;
 
 import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import static com.matt.android.moodtracker_v2.storage.Constants.PREF_KEY_EMPTY_COMMENT;
+import static com.matt.android.moodtracker_v2.storage.Constants.PREF_KEY_MOODLIST;
 
 public class MySharedPreferences {
 
     private HistoryItem historyItem;
+    private ArrayList moodList;
     private SharedPreferences mPreferences;
     public static final String PREF_KEY_NAME = "PREF_KEY_NAME";
     public static final String PREF_KEY_LAST_POS_VIEWPAGER = "PREF_KEY_LAST_POS_VIEWPAGER";
@@ -36,7 +43,7 @@ public class MySharedPreferences {
     public MySharedPreferences(Context context) {
         mPreferences = context.getSharedPreferences(PREF_KEY_NAME, Context.MODE_PRIVATE);
     }
-
+/*
     //Get a date for moods at format dd/mm/yyyy
     public String getMoodDate(Date date) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
@@ -45,7 +52,7 @@ public class MySharedPreferences {
 
     //Save in prefs a mood, with date as key (getMoodDate)
     public void saveMood(Date date, Mood mood) {
-        mPreferences.edit().putString(getMoodDate(date), mood.getTitle()).apply();
+        mPreferences.edit().putString(getMoodDate(date), mood.getTitle().name()).apply();
     }
 
     //Return a mood for a specific date
@@ -56,37 +63,43 @@ public class MySharedPreferences {
             return CurrentMood;
         }
         return null;
+    }  */
+
+    public void saveHistoryItem(Date date, Mood mood) {
+       Gson gson = new Gson();
+       String jsonHistoryItem = gson.toJson(new HistoryItem(date,mood)); // HistoryItem --> Json String
+       mPreferences.edit().putString(getHistoryItemDate(date), jsonHistoryItem).apply(); // Json String --> SharedPrefs
     }
 
-    public void saveHistoryItem(Date date, Mood mood, String comment) {
-       Gson gson = new Gson();
-       String jsonHistoryItem = gson.toJson(new HistoryItem(date,mood,comment)); // HistoryItem --> Json String
-       mPreferences.edit().putString(PREF_KEY_HISTORY_ITEM, jsonHistoryItem).apply(); // Json String --> SharedPrefs
+    public String getHistoryItemDate (Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
+        return "Mood at " + simpleDateFormat.format(date);
     }
 
     //Get historyItemJson and convert it to HistoryItem, then return it
-    public HistoryItem getHistoryItem() {
+    public HistoryItem getHistoryItem(Date date) {
 
         Gson gson = new Gson();
-        String json = mPreferences.getString(PREF_KEY_HISTORY_ITEM,null);
+        String json = mPreferences.getString(getHistoryItemDate(date),null);
         Type type = new TypeToken<HistoryItem>() {}.getType();
-        historyItem = gson.fromJson(json,type);
+        try {
+            historyItem = gson.fromJson(json,type);
+        } catch (IllegalStateException | JsonSyntaxException exception){
+            exception.printStackTrace();
+        }
+
+         try {
+            historyItem.getComment();
+         } catch (NullPointerException e){
+            e.printStackTrace();
+
+        }
         return historyItem;
+      /*  else {
+            historyItem.setComment(PREF_KEY_EMPTY_COMMENT);
+            return historyItem;
+        }   */
 
-    }
-
-    //Same methods for comments
-    public String getCommentDate(Date date) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
-        return "Comment at " + simpleDateFormat.format(date);
-    }
-
-    public void saveComment(Date date, String comment) {
-        mPreferences.edit().putString(getCommentDate(date), comment).apply();
-    }
-
-    public String getComment(Date date) {
-        return mPreferences.getString(getCommentDate(date), null);
     }
 
     public String getMoodNameForSharing(int currentPos) {
@@ -111,7 +124,6 @@ public class MySharedPreferences {
         return moodInStringForShare;
     }
 
-
     //The next methods are for displaying correct last page at each run
     public void saveLastPositionForViewPager(int smileyPos) {
         mPreferences.edit().putInt(PREF_KEY_LAST_POS_VIEWPAGER, smileyPos).apply();
@@ -120,5 +132,6 @@ public class MySharedPreferences {
     public int getLastPositionForViewPager() {
         return mPreferences.getInt(PREF_KEY_LAST_POS_VIEWPAGER, -50);
     }
+
 
 }
