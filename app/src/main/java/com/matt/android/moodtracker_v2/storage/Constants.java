0e50f106d2120e306254808b783204f9c1b1149c
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-25 15:10:16 +0100

    Added constant storage file who will contains all constants

---
+++ app/src/main/java/com/matt/android/moodtracker_v2/storage/Constants.java
@@ -0,0 +1,14 @@
+/*
+ *
+ *   Created by Matteo VACCARI - 2019
+ *
+ *   https://github.com/matteovaccari/MoodTracker
+ *
+ */
+
+package com.matt.android.moodtracker_v2.storage;
+
+public class Constants {
+    public static final String PREF_KEY_EMPTY_COMMENT = "PREF_KEY_EMPTY_COMMENT";
+    public static final String PREF_KEY_MOODLIST = "PREF_KEY_MOODLIST";
+}
