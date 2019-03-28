d1f06774df3c75c24fa0a951fc8128cffd8df87e
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-03-28 13:49:50 +0100

    removed useless attribute from Mood Class

--- app/src/main/java/com/matt/android/moodtracker_v2/models/Mood.java
+++ app/src/main/java/com/matt/android/moodtracker_v2/models/Mood.java
@@ -11,15 +11,11 @@ package com.matt.android.moodtracker_v2.models;
 public class Mood {
 
     private MoodEnum title;
-    private int position;
     private int backgroundColor;
-    private int smiley;
 
-    public Mood(MoodEnum title, int position, int backgroundColor, int smiley) {
+    public Mood(MoodEnum title, int backgroundColor) {
         setTitle(title);
-        setPosition(position);
         setBackgroundColor(backgroundColor);
-        setSmiley(smiley);
     }
 
     public int getBackgroundColor() {
@@ -30,20 +26,12 @@ public class Mood {
         this.backgroundColor = backgroundColor;
     }
 
-    public void setSmiley(int smiley) {
-        this.smiley = smiley;
-    }
-
     public MoodEnum getTitle() {
         return title;
     }
 
     public void setTitle(MoodEnum title) {
         this.title = title;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
     }
 
 }
